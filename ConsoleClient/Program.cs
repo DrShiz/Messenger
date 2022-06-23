2e2cd9d34c33eb805ef3b5c6ad2ba14f285e7f29
Терехов Андрей <shizdryeah@gmail.com>
2022-06-23 18:57:46 +0300

    add JsonConvert

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Newtonsoft.Json;
 
 namespace Messenger
 {
@@ -7,8 +7,13 @@ namespace Messenger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("Shiz", "Whassup", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"Shiz","MessageText":"Whassup","TimeStamp":"2022-06-23T15:56:02.2370438Z"}
+            //Shiz < 6 / 23 / 2022 3:56:02 PM >: Whassup
         }
     }
 }
